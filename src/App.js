dfa8a4565c25811ad8e3427a1e1b51c96c91654e
hann0r <92484605+hann0r@users.noreply.github.com>
2022-03-23 17:42:26 +0000

    Added test button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Dictionary</header>
+      <a href="#" className="btn btn-primary">
+        {" "}
+        Hello{" "}
+      </a>
     </div>
   );
 }
