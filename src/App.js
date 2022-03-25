31a2b211a124eefd549f09e0d2b3e771fdfc7d8d
hann0r <92484605+hann0r@users.noreply.github.com>
2022-03-25 16:51:28 +0000

    Added Search element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import React from "react";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Dictionary</header>
+      <Dictionary />
     </div>
   );
 }
