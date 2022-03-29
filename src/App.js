a9e66cca156862a83bf44f93a66ff194242891e6
hann0r <92484605+hann0r@users.noreply.github.com>
2022-03-30 00:45:08 +0100

    Added extra styling

--- src/App.js
+++ src/App.js
@@ -9,6 +9,26 @@ function App() {
         <span className="App-title">Dictionary</span>
       </header>
       <Dictionary />
+      <footer>
+        Coded by <strong>Hannah Harries</strong> <br />
+        <br />
+        <a
+          href="https://github.com/hann0r/dictionary-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-sourced on GitHub
+        </a>
+        <br />
+        <br />
+        <a
+          href="https://adorable-lokum-6143fe.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
