492789c8823d92f14501bd07907740ddf992dbf7
hann0r <92484605+hann0r@users.noreply.github.com>
2022-03-29 21:07:33 +0100

    Added Photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -8,10 +9,14 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+  function handlePexelsResponse(respose) {
+    setPhotos(respose.data.photos);
   }
 
   function search(event) {
@@ -20,10 +25,17 @@ export default function Dictionary() {
     // documentation: https://api.dictionaryapi.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001b94689f5adae4815a33c5fd52078e191";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search} className="Search">
@@ -36,6 +48,9 @@ export default function Dictionary() {
       <strong className="Suggest">Suggested words:</strong> Earth, Moon,
       Peace...
       <Results results={results} />
+      <section>
+        <Photos photos={photos} />
+      </section>
     </div>
   );
 }
