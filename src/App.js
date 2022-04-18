b6b6fbbdbc3a5dbf44cd78a220bb3d2c217d4f44
hann0r <92484605+hann0r@users.noreply.github.com>
2022-04-18 16:48:30 +0100

    changed margins

--- src/App.js
+++ src/App.js
@@ -12,38 +12,47 @@ function App() {
       </header>
 
       <Dictionary defaultKeyword="galaxy" />
-      <footer>
-        <FontAwesomeIcon icon={faHandPointUp} />
-        <p className="Original">
-          <em>
-            <strong>Click images</strong> for original source{" "}
-          </em>
-        </p>
-        <br />
-        <br />
-        Coded by{" "}
-        <strong>
-          <u>Hannah Harries</u>
-        </strong>
-        <br />
-        <br />
-        <a
-          href="https://github.com/hann0r/dictionary-project"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open-sourced on GitHub
-        </a>
-        <br />
-        <br />
-        <a
-          href="https://adorable-lokum-6143fe.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hosted on Netlify
-        </a>
-      </footer>
+
+      <div class="container ms-1000">
+        <div class="row">
+          <div class="col-sm-4">
+            <footer>
+              <p className="Github">
+                <FontAwesomeIcon icon={faHandPointUp} />
+                <p className="Original">
+                  <em>
+                    <strong>Click images</strong> for original source{" "}
+                  </em>
+                </p>
+                <br />
+                <br />
+                Coded by{" "}
+                <strong>
+                  <u>Hannah Harries</u>
+                </strong>
+                <br />
+                <br />
+                <a
+                  href="https://github.com/hann0r/dictionary-project"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Open-sourced on GitHub
+                </a>
+                <br />
+                <br />
+                <a
+                  href="https://adorable-lokum-6143fe.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Hosted on Netlify
+                </a>
+              </p>
+            </footer>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
